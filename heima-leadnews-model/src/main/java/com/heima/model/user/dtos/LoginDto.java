56716715900b663348757beea1abf5ac14958b0b
box <1214142612@qq.com>
2022-09-02 12:58:01 +0800

    继承swagger和nif4j

---
+++ heima-leadnews-model/src/main/java/com/heima/model/user/dtos/LoginDto.java
@@ -0,0 +1,22 @@
+package com.heima.model.user.dtos;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author 12141
+ */
+@Data
+public class LoginDto {
+    /**
+     * 手机号
+     */
+    @ApiModelProperty(value = "手机号", required = true)
+    private String phone;
+
+    /**
+     * 密码
+     */
+    @ApiModelProperty(value = "密码", required = true)
+    private String password;
+}

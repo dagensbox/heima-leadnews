fdbca642542ec025ae16fd159be49965900e5284
box <1214142612@qq.com>
2022-09-05 10:50:40 +0800

    minio集成完成

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleContentMapper.java
@@ -0,0 +1,10 @@
+package com.heima.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.article.pojos.ApArticleContent;
+
+/**
+ * @author 12141
+ */
+public interface ApArticleContentMapper extends BaseMapper<ApArticleContent> {
+}
---
+++ heima-leadnews-service/heima-leadnews-article/src/test/java/com/heima/article/test/ArticleFreemarkerTest.java
@@ -0,0 +1,68 @@
+package com.heima.article.test;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.alibaba.fastjson.JSONArray;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.heima.article.ArticleApplication;
+import com.heima.article.mapper.ApArticleContentMapper;
+import com.heima.article.mapper.ApArticleMapper;
+import com.heima.file.service.FileStorageService;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.article.pojos.ApArticleContent;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@SpringBootTest(classes = {ArticleApplication.class})
+public class ArticleFreemarkerTest {
+
+    @Autowired
+    private ApArticleMapper apArticleMapper;
+
+    @Autowired
+    private ApArticleContentMapper apArticleContentMapper;
+
+    @Autowired
+    private Configuration configuration;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Test
+    void createStaticUrlTest() throws Exception {
+        //1、获取文章内容
+        ApArticleContent apArticleContent = apArticleContentMapper.selectOne(Wrappers.<ApArticleContent>lambdaQuery()
+                .eq(ApArticleContent::getArticleId, 1302862387124125698L));
+
+        if (apArticleContent != null && StringUtils.isNotBlank(apArticleContent.getContent())){
+            //2、文章内容通过freemarker生成html
+            StringWriter out = new StringWriter();
+            Template template = configuration.getTemplate("article.ftl");
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("content", JSONArray.parseArray(apArticleContent.getContent()));
+
+            template.process(params,out);
+
+            InputStream is = new ByteArrayInputStream(out.toString().getBytes());
+
+            //3、把html上传到minio中
+            String path = fileStorageService.uploadHtmlFile("", apArticleContent.getArticleId() + ".html", is);
+
+            //4、修改ap_article表,保存static_url字段
+            ApArticle apArticle = new ApArticle();
+            apArticle.setId(apArticleContent.getArticleId());
+            apArticle.setStaticUrl(path);
+            apArticleMapper.updateById(apArticle);
+        }
+    }
+}

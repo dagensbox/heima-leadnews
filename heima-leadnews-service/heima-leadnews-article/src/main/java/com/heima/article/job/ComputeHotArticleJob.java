665971607db7b00f999930582002f17149cd8d09
box <1214142612@qq.com>
2022-09-13 15:29:22 +0800

    添加xxl完成

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/job/ComputeHotArticleJob.java
@@ -0,0 +1,26 @@
+package com.heima.article.job;
+
+import com.heima.article.service.HotArticleService;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 12141
+ */
+@Component
+@Slf4j
+public class ComputeHotArticleJob {
+    @Autowired
+    private HotArticleService hotArticleService;
+
+
+    @XxlJob("computeHotArticleJob")
+    public void handleComputeHotArticleJob() {
+        log.info("热文章分值计算调度任务开始执行...");
+        hotArticleService.computeHotArticle();
+        log.info("热文章分值计算调度任务结束...");
+    }
+
+}
